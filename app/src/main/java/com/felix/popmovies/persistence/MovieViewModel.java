74735a21043370afbfefad7175d8c29f35caa012
Felix Lin <swift.python.geeks@gmail.com>
2019-05-05 00:09:00 -0700

    Add ViewModel

---
+++ app/src/main/java/com/felix/popmovies/persistence/MovieViewModel.java
@@ -0,0 +1,29 @@
+package com.felix.popmovies.persistence;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import com.felix.popmovies.model.Movie;
+
+import java.util.List;
+
+public class MovieViewModel extends AndroidViewModel {
+
+    private MovieRepository repository;
+    private LiveData<List<Movie>> favoriteMovies;
+
+    public MovieViewModel(Application application) {
+        super(application);
+        repository = new MovieRepository(application);
+        favoriteMovies = repository.getFavoriteMovies();
+    }
+
+    LiveData<List<Movie>> getFavoriteMovies() {
+        return favoriteMovies;
+    }
+
+    public void insert(Movie movie) {
+        repository.insert(movie);
+    }
+}
